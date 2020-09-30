520a27fd8e5e265db8d463c37cf07830e4ac9547
Marc Auberer <marc.auberer@chillibits.com>
2020-09-30 18:16:49 +0200

    Add base files & gh workflows

---
+++ src/app/_pages/sign-survey/sign-survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-survey',
+  templateUrl: './sign-survey.component.html',
+  styleUrls: ['./sign-survey.component.sass']
+})
+export class SignSurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './_pages/main/main.component';
+import { SignInComponent } from './_pages/sign-in/sign-in.component';
+import { SignUpComponent } from './_pages/sign-up/sign-up.component';
+import { SignSurveyComponent } from './_pages/sign-survey/sign-survey.component';
+import { ToolbarComponent } from './_component/toolbar/toolbar.component';
+import { ButtonComponent } from './_component/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SignInComponent,
+    SignUpComponent,
+    SignSurveyComponent,
+    ToolbarComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
