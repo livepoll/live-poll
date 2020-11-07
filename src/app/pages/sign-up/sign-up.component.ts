b12767f96236cd66313938db60423b289229f045
Marc Auberer <marc.auberer@chillibits.com>
2020-11-07 23:56:28 +0100

    Add sign up screen base layout

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../model/user';
+import {SHA256} from 'crypto-js';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,6 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  ngOnInit(): void {}
+  validateForm!: FormGroup;
+  username: string;
+  email: string;
+  password: string;
+  loading = false;
 
+  /**
+   * Initialize login component
+   *
+   * @param formBuilder Injected form builder
+   */
+  constructor(private formBuilder: FormBuilder) {}
+
+  /**
+   * Initialize form validation
+   */
+  ngOnInit(): void {
+    this.validateForm = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      agb: [false, [Validators.required]]
+    });
+  }
+
+  /**
+   * Validates the sign up form
+   */
+  submitForm(): void {
+    // Validate form controls
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    if (this.validateForm.valid) {
+      this.loading = true;
+      this.signUp(this.username, this.email, this.password);
+    }
+  }
+
+  /**
+   * Sign up
+   */
+  signUp(username: string, email: string, password: string): void {
+
+  }
 }
--- src/app/pages/sign-up/sign-up.module.ts
+++ src/app/pages/sign-up/sign-up.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { SignUpComponent } from './sign-up.component';
 import { SignUpRoutingModule } from './sign-up-routing.module';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {NzCheckboxModule} from 'ng-zorro-antd/checkbox';
 
 @NgModule({
   imports: [
-    SignUpRoutingModule
+    SignUpRoutingModule,
+    NzCardModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    ReactiveFormsModule,
+    NzCheckboxModule
   ],
   declarations: [SignUpComponent],
   exports: [SignUpComponent]
