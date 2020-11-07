6362f81b726cc866e228da93a6e155cd5d074ded
Marc Auberer <marc.auberer@chillibits.com>
2020-11-07 22:17:13 +0100

    Finish login process

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SHA256 } from 'crypto-js';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'app-login',
@@ -8,22 +10,49 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  @Output() login = new EventEmitter<User>();
+
   validateForm!: FormGroup;
+  username: string;
+  password: string;
+  remember = true;
+  loading = false;
 
-  constructor(private fb: FormBuilder) {}
+  /**
+   * Initialize login component
+   *
+   * @param formBuilder Injected form builder
+   */
+  constructor(private formBuilder: FormBuilder) {}
 
+  /**
+   * Initialize form validation
+   */
   ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
+    this.validateForm = this.formBuilder.group({
+      username: [null, [Validators.required]],
       password: [null, [Validators.required]],
       remember: [true]
     });
   }
 
+  /**
+   * Validates the login form
+   */
   submitForm(): void {
+    // Validate form controls
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
+    }
+    if (this.validateForm.valid) {
+      this.loading = true;
+      // Create user instance and trigger login
+      const user = new User();
+      user.username = this.username;
+      user.password = SHA256(this.password).toString();
+      user.accountState = this.remember ? 1 : 0;
+      this.login.emit(user);
     }
   }
 }
