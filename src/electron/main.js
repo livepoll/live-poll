eb6b0d1bcb62d8f7d4f60ef3f320da6d11ddf2ec
Marc Auberer <marc.auberer@sap.com>
2020-11-23 10:39:29 +0100

    Fix electron build bug

--- src/electron/main.js
+++ src/electron/main.js
@@ -14,19 +14,22 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, '../index.html'),
+    pathname: path.join(__dirname, '../../dist/live-poll/index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
