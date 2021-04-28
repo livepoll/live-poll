090b03f4f59d599585a6265473e07789ba0b46d6
Marc Auberer <marc.auberer@sap.com>
2021-04-28 20:52:32 +0200

    Fine-tuning

--- src/app/service/websocket.service.ts
+++ src/app/service/websocket.service.ts
@@ -2,6 +2,9 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { webSocket } from 'rxjs/webSocket';
 import {environment as env} from '../../environments/environment';
+import {QuizItemAnswer} from '../model/quiz-item-answer';
+import {MultipleChoiceItemAnswer} from '../model/multiple-choice-item-answer';
+import {OpenTextItemAnswer} from '../model/open-text-item-answer';
 
 const ENDPOINT_WEBSOCKET_URL = env.apiBaseWebsocketUrl + '/websocket';
 const ENDPOINT_HANDSHAKE_URL = ENDPOINT_WEBSOCKET_URL + '/enter-poll';
@@ -13,6 +16,7 @@ const ENDPOINT_MESSAGING_WRITE_URL = ENDPOINT_WEBSOCKET_URL + '/answer';
 })
 export class WebsocketService {
 
+  private webSocket;
   private subject;
 
   /**
@@ -27,17 +31,36 @@ export class WebsocketService {
    * Tries to connect via the handshake endpoint of the websocket server
    */
   establishConnection(slug: string): void {
-    this.subject = webSocket({
+    this.webSocket = webSocket({
       url: ENDPOINT_HANDSHAKE_URL,
-      deserializer: ({data}) => data
+      deserializer: ({data}) => data,
+      serializer: msg => JSON.stringify(msg),
+      openObserver: {
+        next: () => console.log('socket connection established')
+      },
+      closeObserver: {
+        next: () => console.log('socket connection closed')
+      }
     });
-    this.subject.subscribe(console.log);
+    this.subject = this.webSocket.subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  /**
+   * Sends an answer via the websocket to the server
+   *
+   * @param answer Answer object, which will be serialized
+   */
+  sendAnswer(answer: MultipleChoiceItemAnswer|QuizItemAnswer|OpenTextItemAnswer): void {
+    this.subject?.next(answer);
   }
 
   /**
    * Closes an established connection
    */
   closeConnection(): void {
-    this.subject?.closeConnection();
+    this.subject.unsubscribe();
+    this.webSocket?.complete();
   }
 }
