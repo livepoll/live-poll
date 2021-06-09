e24a510719eaaf250196c56e13de0f32d3173a59
Marc Auberer <marc.auberer@chillibits.com>
2021-06-09 15:02:30 +0200

    Fix localhost problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   name: 'production',
   production: true,
-  apiBaseWebsocketUrl: 'wss://localhost:8080/v1',
+  apiBaseWebsocketUrl: 'wss://api.live-poll.de/v1',
   apiBaseUrl: `https://api.live-poll.de/v1`,
   useSecureCookies: true,
   cookieConsentUrl: 'www.live-poll.de'
