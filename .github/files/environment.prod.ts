a7dbc7a363e40805a185d42e29ae9e31ce5c68b6
Marc Auberer <marc.auberer@chillibits.com>
2021-06-14 01:27:48 +0200

    Add compose files for delivery (#328)
    * Adjust workflow to build localhost image
    
    * Add scripts for delivery

---
+++ .github/files/environment.prod.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright © Live-Poll 2020-2021. All rights reserved
+ */
+
+export const environment = {
+  name: 'production',
+  production: true,
+  apiBaseWebsocketUrl: 'ws://localhost/v1',
+  apiBaseUrl: 'http://localhost/v1',
+  useSecureCookies: true,
+  cookieConsentUrl: 'www.live-poll.de'
+};
---
+++ .github/files/environment.staging.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright © Live-Poll 2020-2021. All rights reserved
+ */
+
+export const environment = {
+  name: 'staging',
+  production: true,
+  apiBaseWebsocketUrl: 'ws://localhost:8080/v1',
+  apiBaseUrl: 'http://localhost:8080/v1',
+  useSecureCookies: true,
+  cookieConsentUrl: 'dev.live-poll.de'
+};
